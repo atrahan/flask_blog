d4047f3995fb275fa17935efc374716047fce1d5
atrahan <alextrahan@gmail.com>
2019-09-25 15:28:07 -0700

    Add login and registration with form field checking

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+with open('skey.txt','r') as f:
+    app.config['SECRET_KEY'] = f.read()
 
 posts = [
     {
@@ -24,6 +28,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(message=f'Account created for {form.username.data}!', category='success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'pie@app.com' and form.password.data == 'pie':
+            flash(message=f'You have been logged in!', category='success')
+            return redirect(url_for('home'))
+        else:
+            flash(message='Login Unseccessful. Please check user/pwd.', category='danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
