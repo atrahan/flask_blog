9fb0d1471d97971a600f8a1349748e2ce6d3008e
atrahan <alextrahan@gmail.com>
2020-02-01 17:17:26 -0800

    Add users to database upon registration

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 with open('skey.txt','r') as f:
     app.config['SECRET_KEY'] = f.read()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
