a17fa73fdc881f7c5cae9a44a125d8060ad2c5f2
atrahan <alextrahan@gmail.com>
2020-02-03 21:23:15 -0800

    Adding logout options and account page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 with open('skey.txt','r') as f:
@@ -8,5 +9,8 @@ with open('skey.txt','r') as f:
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
